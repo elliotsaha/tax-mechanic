bc22fa419408a418ee6f93eb56d7167e1109d2fb
Heril Saha <herilsaha06@gmail.com>
2020-05-10 00:04:26 -0600

    Testing Hover with Navbar Spans

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -6,12 +6,14 @@ export default class Homepage extends Component {
     render() {
         return (
             <div className="MainHomepageBody">
+                {/* 
                 <Section1 />
                 <Section2 />
                 <Section3 />
                 <Section4 />
                 <Section5 />
                 <Section6 />
+                */}   
             </div>
         )
     }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
     return (
         <React.Fragment>
-        <ReactBootStrap.Navbar className="NavbarBackground" expand="lg">
+        <ReactBootStrap.Navbar className="NavbarBackground" expand="xl">
         <ReactBootStrap.Navbar.Brand><Link to="/" className="Logo"><img src="./img/TaxMechanicLogo.png" width="200px" alt="Tax Mechanic Logo"/></Link></ReactBootStrap.Navbar.Brand>
         
         <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -14,10 +14,10 @@ export default function Navbar() {
             <ReactBootStrap.Nav className="mr-auto">
                     
                 
-            <ReactBootStrap.Nav.Link><Link to="/" className="NavbarLink">HOME</Link></ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link><Link to="/ourteam" className="NavbarLink">OUR TEAM</Link></ReactBootStrap.Nav.Link> 
+            <ReactBootStrap.Nav.Link><Link to="/" className="NavbarLink"><span>HOME</span></Link></ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link><Link to="/ourteam" className="NavbarLink"><span>OUR TEAM</span></Link></ReactBootStrap.Nav.Link> 
             
-            <ReactBootStrap.NavDropdown title={<span className="NavbarLink">SERVICES</span>}  id="basic-nav-dropdown">
+            <ReactBootStrap.NavDropdown className="Dropdown" title={<span className="NavbarLink">SERVICES</span>}  id="basic-nav-dropdown">
                 <ReactBootStrap.NavDropdown.Item><Link to="/services">ALL SERVICES</Link></ReactBootStrap.NavDropdown.Item>
                 <ReactBootStrap.NavDropdown.Divider />
                 <ReactBootStrap.NavDropdown.Item><Link to="/services/taxrepresentation">TAX REPRESENTATION</Link></ReactBootStrap.NavDropdown.Item>
@@ -30,10 +30,10 @@ export default function Navbar() {
                 <ReactBootStrap.NavDropdown.Item><Link to="/services/ongoingsupport">ONGOING SUPPORT</Link></ReactBootStrap.NavDropdown.Item>
                 <ReactBootStrap.NavDropdown.Item><Link to="/services/crataxreduction">CRA TAX REDUCTION</Link></ReactBootStrap.NavDropdown.Item>
             </ReactBootStrap.NavDropdown>
-            <ReactBootStrap.Nav.Link><Link to="/blog" className="NavbarLink">BLOG</Link></ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link><Link to="ebook" className="NavbarLink">E-BOOK</Link></ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link><Link to="testimonials" className="NavbarLink">TESTIMONIALS</Link></ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link><Link to="contact" className="NavbarLink">CONTACT US</Link></ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link><Link to="/blog" className="NavbarLink"><span>BLOG</span></Link></ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link><Link to="ebook" className="NavbarLink"><span>E-BOOK</span></Link></ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link><Link to="testimonials" className="NavbarLink"><span>TESTIMONIALS</span></Link></ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link><Link to="contact" className="NavbarLink"><span>CONTACT US</span></Link></ReactBootStrap.Nav.Link>
             </ReactBootStrap.Nav>
             
         </ReactBootStrap.Navbar.Collapse>
