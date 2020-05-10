db6079a6e1663db8b9e59350f41575c2c2f82bb6
Heril Saha <herilsaha06@gmail.com>
2020-05-10 00:45:38 -0600

    Animated hover on Navbar elements, starting on navbar dropdown elements

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,26 +14,28 @@ export default function Navbar() {
             <ReactBootStrap.Nav className="mr-auto">
                     
                 
-            <ReactBootStrap.Nav.Link><Link to="/" className="NavbarLink"><span>HOME</span></Link></ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link><Link to="/ourteam" className="NavbarLink"><span>OUR TEAM</span></Link></ReactBootStrap.Nav.Link> 
+            <ReactBootStrap.Nav.Link><Link to="/" className="NavbarLink"><a>HOME</a></Link></ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link><Link to="/ourteam" className="NavbarLink"><a>OUR TEAM</a></Link></ReactBootStrap.Nav.Link> 
             
-            <ReactBootStrap.NavDropdown className="Dropdown" title={<span className="NavbarLink">SERVICES</span>}  id="basic-nav-dropdown">
-                <ReactBootStrap.NavDropdown.Item><Link to="/services">ALL SERVICES</Link></ReactBootStrap.NavDropdown.Item>
-                <ReactBootStrap.NavDropdown.Divider />
-                <ReactBootStrap.NavDropdown.Item><Link to="/services/taxrepresentation">TAX REPRESENTATION</Link></ReactBootStrap.NavDropdown.Item>
-                <ReactBootStrap.NavDropdown.Item><Link to="/services/taxappeal">TAX APPEAL</Link></ReactBootStrap.NavDropdown.Item>
-                <ReactBootStrap.NavDropdown.Item><Link to="/services/latetaxfilings">LATE TAX FILINGS</Link></ReactBootStrap.NavDropdown.Item>
-                <ReactBootStrap.NavDropdown.Item><Link to="/services/taxrelief">TAX RELIEF</Link></ReactBootStrap.NavDropdown.Item>
-                <ReactBootStrap.NavDropdown.Item><Link to="/services/voluntarydisclosure">VOLUNTARY DISCLOSURE</Link></ReactBootStrap.NavDropdown.Item>
-                <ReactBootStrap.NavDropdown.Item><Link to="/services/taxaudits">TAX AUDITS</Link></ReactBootStrap.NavDropdown.Item>
-                <ReactBootStrap.NavDropdown.Item><Link to="/services/taxpaymentplan">TAX PAYMENT PLAN</Link></ReactBootStrap.NavDropdown.Item>
-                <ReactBootStrap.NavDropdown.Item><Link to="/services/ongoingsupport">ONGOING SUPPORT</Link></ReactBootStrap.NavDropdown.Item>
-                <ReactBootStrap.NavDropdown.Item><Link to="/services/crataxreduction">CRA TAX REDUCTION</Link></ReactBootStrap.NavDropdown.Item>
+            <ReactBootStrap.NavDropdown title={<div className="NavbarLink"><a>SERVICES</a></div>}  id="basic-nav-dropdown">
+                <div className="DropdownChildren">
+                    <ReactBootStrap.NavDropdown.Item><Link to="/services">ALL SERVICES</Link></ReactBootStrap.NavDropdown.Item>
+                    <ReactBootStrap.NavDropdown.Divider />
+                    <ReactBootStrap.NavDropdown.Item><Link to="/services/taxrepresentation">TAX REPRESENTATION</Link></ReactBootStrap.NavDropdown.Item>
+                    <ReactBootStrap.NavDropdown.Item><Link to="/services/taxappeal">TAX APPEAL</Link></ReactBootStrap.NavDropdown.Item>
+                    <ReactBootStrap.NavDropdown.Item><Link to="/services/latetaxfilings">LATE TAX FILINGS</Link></ReactBootStrap.NavDropdown.Item>
+                    <ReactBootStrap.NavDropdown.Item><Link to="/services/taxrelief">TAX RELIEF</Link></ReactBootStrap.NavDropdown.Item>
+                    <ReactBootStrap.NavDropdown.Item><Link to="/services/voluntarydisclosure">VOLUNTARY DISCLOSURE</Link></ReactBootStrap.NavDropdown.Item>
+                    <ReactBootStrap.NavDropdown.Item><Link to="/services/taxaudits">TAX AUDITS</Link></ReactBootStrap.NavDropdown.Item>
+                    <ReactBootStrap.NavDropdown.Item><Link to="/services/taxpaymentplan">TAX PAYMENT PLAN</Link></ReactBootStrap.NavDropdown.Item>
+                    <ReactBootStrap.NavDropdown.Item><Link to="/services/ongoingsupport">ONGOING SUPPORT</Link></ReactBootStrap.NavDropdown.Item>
+                    <ReactBootStrap.NavDropdown.Item><Link to="/services/crataxreduction">CRA TAX REDUCTION</Link></ReactBootStrap.NavDropdown.Item>
+                </div>
             </ReactBootStrap.NavDropdown>
-            <ReactBootStrap.Nav.Link><Link to="/blog" className="NavbarLink"><span>BLOG</span></Link></ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link><Link to="ebook" className="NavbarLink"><span>E-BOOK</span></Link></ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link><Link to="testimonials" className="NavbarLink"><span>TESTIMONIALS</span></Link></ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link><Link to="contact" className="NavbarLink"><span>CONTACT US</span></Link></ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link><Link to="/blog" className="NavbarLink"><a>BLOG</a></Link></ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link><Link to="ebook" className="NavbarLink"><a>E-BOOK</a></Link></ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link><Link to="testimonials" className="NavbarLink"><a>TESTIMONIALS</a></Link></ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link><Link to="contact" className="NavbarLink"><a>CONTACT US</a></Link></ReactBootStrap.Nav.Link>
             </ReactBootStrap.Nav>
             
         </ReactBootStrap.Navbar.Collapse>
