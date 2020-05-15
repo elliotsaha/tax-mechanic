634a49f11cc2be83422d85068b6dd5e99830c215
HerilS <herilsaha06@gmail.com>
2020-05-15 03:53:31 -0600

    Finished Section 4 Mobile View, Re-editing PC View

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -6,13 +6,13 @@ export default class Homepage extends Component {
     render() {
         return (
             <div className="MainHomepageBody"> 
-            <Section1 /><Section2 /><Section3 /><Section4 />
+            <Section1 /><Section2 /><Section3 /><Section4 /><Section5 />
                 {/* 
                
                 
                 
                 
-                <Section5 />
+                
                 <Section6 />
                 */}   
             </div>
