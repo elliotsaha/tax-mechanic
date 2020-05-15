7ece65d2ee42d8e067e74928a1a8c54d134ae371
HerilS <herilsaha06@gmail.com>
2020-05-15 09:40:20 -0600

    Fixed Mobile View for S1 (Used Different Image), Fixing Comp View for S1

--- src/components/Homepage/HomepageSections.js
+++ src/components/Homepage/HomepageSections.js
@@ -7,12 +7,13 @@ import Service from "../Misc/Service/Service";
 export function Section1() {
     return (
         <div className="S1">
-            <img className="GradientS1" src="./img/Homepage/TopWork.jpg" width="100%" alt="Homepage"/>
+            <img className="GradientS1Mobile" src="./img/Homepage/MobileTopWork.png" width="100%" alt="Homepage" />
+            <img className="GradientS1Comp" src="./img/Homepage/TopWork.jpg" width="100%" alt="Homepage"/>
             <div className="S1TextContainer">
                 <div className="S1Text">
                     <span>We Fix Your Tax <br /> Problems. </span>
-                    <div>
-                        <Button text="Book For a Free Consultation Today"/>
+                    <div className="ButtonContainerS1">
+                        <Button className="Button" text="Book For a Free Consultation Today"/>
                     </div>
                 </div>
             </div>
