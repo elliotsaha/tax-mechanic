8f7d0d937c8c1d06f9eeb6e3bba93507c9005889
HerilS <herilsaha06@gmail.com>
2020-05-15 12:12:45 -0600

    Fixed Static Footer Without Switch in Homepage

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import "../../Sass/Homepage/Homepage/Homepage.css";
-import { Section1, Section2, Section3, Section4, Section5, Section6 } from "./HomepageSections";
+import { Section1, Section2, Section3, Section4, Section5 } from "./HomepageSections";
 
 export default class Homepage extends Component {
     render() {
         return (
             <div className="MainHomepageBody"> 
-            <Section1 /><Section2 /><Section3 /><Section4 /><Section5 /><Section6 />
+            <Section1 /><Section2 /><Section3 /><Section4 /><Section5 />
                 {/* 
                
                 
--- src/components/Homepage/HomepageSections.js
+++ src/components/Homepage/HomepageSections.js
@@ -128,34 +128,4 @@ export function Section5() {
     )
 }
 
-// Section 6
-export function Section6() {
-    return (
-        <div className="S6">
-            <div className="TopS6">
-            <p className="p1S6"> 
-                Location <br />
-                180 John Street <br />
-                Toronto, ON <br />
-                M5T 1X5 <br />
-            </p>
 
-            <p className="p2S6">TEL: 647-499-5693 <br/>
-            FAX: 647-494-0169</p>
-
-            <p className="p3S6">Email: <br/>david@taxmechanic.ca
-            </p>
-            <div className="img4S6Container">
-                <img src="./img/TaxMechanicLogo.png" alt="Logo"/>
-            </div>
-           
-        </div>
-        <div className="ButtonContainer">
-            <span
-            >&copy;
-            2020 Tax Filing Services Toronto – Taxmechanic.ca. All Rights Reserved.</span>
-        </div>
-        
-        </div>
-    )
-}
