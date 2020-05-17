ab35ef1e92b48aae14fd50a52a8788e9203923fc
HerilS <herilsaha06@gmail.com>
2020-05-16 23:40:58 -0600

    Starting Section 2 on OurTeam Page

--- client/src/components/Misc/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "./Button.scss";
-
-export default function Button({text}) {
-    return (
-        <button className="Button">{text}<p className="Arrow">&#8594;</p></button>
-    )
-}
