8700b08b6c41a83dd3cdb8ad8edffbcd4c240cdc
HerilS <herilsaha06@gmail.com>
2020-05-23 04:18:00 -0600

    Fixed Formating for Blog

---
+++ src/components/Blog/Page1/P1.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import BlogPages from "../../Misc/BlogPages/BlogPages";
+import Button from "../../Misc/Button/Button";
+export default function P1() {
+    return (
+            <div className="BlogMain">
+                <div className="BlogS1">
+            <img className="GradientS1Mobile" src="/img/OurTeam/TopImgMobile.png" width="100%" alt="Homepage" />
+            <img className="GradientS1Comp" src="/img/OurTeam/TopImg.png" width="100%" alt="OurTeam"/>
+                <div className="S1TextContainer">
+                    <div className="S1Text">
+                        <span style={{whiteSpace: "normal"}}>Underground Economy In a Pandemic</span>
+                        <div className="ButtonContainerS1">
+                        <Button className="Button" text="Book For a Free Consultation Today"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <BlogPages date="May 19, 2020" title="Underground Economy In A Pandemic" subtitle="By: Fraser Simpson" mainPara=
+            {
+            <div>
+            <p>A white paper is an authoritative document intended to fully inform the reader on a particular topic. It combines expert knowledge and research into a document that argues for a specific solution or recommendation. The white paper allows the reader to understand an issue, solve a problem, or make a decision.
+            Covid-19 has changed the landscape. Revenue Canada has adapted, and I am proud to say for the better. Thousands of CRA officers have volunteered to take calls from worried taxpayers trying to apply for various emergency benefit programs. These officers are Canadians first, employees second, and are averaging forty calls a day. Makes me damn proud to be a Canadian!
+            What does this mean for you? Well if you are reading this it is because you have a problem. You have not filed your taxes for a number of years. Solving this problem just got easier. Here are five reasons why you should file now;</p>
+            <ul>
+                <li>Revenue Canada has suspended collection action.</li>
+                <li>You do not have to pay tax debts for six months.</li>
+                <li>Revenue Canada is focusing on CERB and helping Canadians.</li>
+                <li>Negotiations resulting in more favourable outcomes.</li>
+                <li>Tax Mechanic is offering flexible payment options.</li>
+            </ul>
+            <span>A fast-changing job</span>
+            <p>
+                Remember the days when you had to listen to two minutes of dial-up noises to connect to the internet? Even checking your email was such a hassle. Nowadays, we can be online with literally only the tip of our fingertips. Just tap our phones and voila–we have access to the internet and the gazillions of apps and web services associated with it.
+                Accompanying this drastic change in &#40;and need for&#41; technology over the last couple of decades, the role of a tax professional has evolved significantly. It’s no longer just blindly producing tax returns all day long. The daily challenges that a tax professional encounters have become much more multidimensional: constantly solving complex problems, keeping up to date with new technologies and methodologies, while also staying innovative and ethical.
+                At the Tax Mechanic, we know what taxpayers want. Our mission is to empower them to learn how to file, without friction or penalties, no matter their educational or professional background. By listening to the needs of our community of five thousand taxpayers since 1985, we’ve come to fully understand what drives them.
+                Are you part of the “hidden economy?”
+                <br/> <br/>
+                Recently the CRA won a court decision forcing a roofing supply company to give over customer records. The federal agency uses data to fight tax evasion in the construction sector. The Canada Revenue Agency &#40;CRA&#41; can compel a third party to divulge information about its customers, according to a decision released earlier this week by the Federal Court of Appeal &#40;FCA&#41; In Roofmart Ontario Inc. V Canada &#40;National Revenue&#41;, the appellant, Roofmart, a supplier of roofing and building materials, lost its appeal of a Federal Court decision granting a request by the CRA to impose an order on Roofmart to disclose customer information. The request is colloquially known as an “unnamed persons requirement” &#40;UPR&#41; under the Income Tax Act &#40;ITA&#41;. <br/>
+                In recent years, the CRA has made it a greater priority to target the “hidden economy” by dedicating more resources toward combatting non-compliance. According to an Ontario Ministry of Labour study cited in the decision, 28% of the construction industry’s economic activity was unreported or underreported, and as much as one-fifth of residential construction takes place unreported. <br/>
+                The CRA made an UPR application targeting Roofmart customers who spent $20,000 or more between 2015 and 2017, and $10,000 or more in the first six months of 2018.
+                Justice Rennie granted the UPR, determining that the conditions needed had been satisfied.
+                The Minister sought the following information related to the group of persons described above: <br /> <br />
+                a&#41; The Customers’ legal name, business or operating name, contact person, business address, postal code, and all telephone numbers on file; <br />
+                b&#41; The Customers’ business number, if known; <br/>
+                c&#41; The Customers’ itemized transaction details including invoice date, invoice number, total sale amount, method of payment, and address of delivery; and <br />
+                d&#41; All bank account information for the Customers &#40;including transit, institution, and account numbers&#41; from credit applications and/or otherwise maintained by Roofmart in its records. <br /> <br />
+                Justice Rennie concluded; <br />
+                “I do not accept this limitation on the discretion of the Court to grant a UPR. Canada has a system of self-assessment and self-reporting of income. It depends on the integrity and honesty of taxpayers to be fair and efficient &#40;R. v. McKinlay Transport Ltd., &#91;1990&#93; 1 S.C.R. 627 at 636&#41;. Parliament has granted the Minister corresponding powers to verify and test compliance. These powers lie at the heart of the Minister’s ability to enforce taxation legislation. The broader public interest in the enforcement of our system of taxation outweighs the appellant’s private and commercial interests in not disclosing its clients’ personal information &#40;eBay Canada Limited v. Canada &#40;National Revenue&#41;, 2008 FCA 141 at para. 39 &#40;eBay II&#41;&#41;. This Court’s response in eBay I to similar policy arguments regarding taxpayer privacy bears repeating here &#40;eBay I at para. 67&#41;:
+                In a self-reporting system of taxation, “&#91;t&#93;axpayers have a very low expectation of privacy in their business records relevant to the determination of their tax liability” &#40;Redeemer Foundation v. Canada &#40;Minister of National Revenue&#41;, 2008 SCC 46 at para. 25&#41; and a requirement “provides the least intrusive means by which effective monitoring of compliance with the Income Tax Act can be effected” &#40;R. v. McKinlay Transport Ltd., supra at 649&#41;.”<br/><br/>
+
+                In 2017, the Federal Court of Appeal ruled against hardware giant Rona in another UPR initiated by the CRA. Rona sought leave to appeal to the Supreme Court of Canada, but the leave was denied.
+                    <br /><br/>
+                Read the case brief here.
+                    <br/><br/>
+                My experience has been that a lot of entrepreneurs get into trouble by not maintaining their accounting records and as a result they fall behind on their taxes. Is that you? If so, you have options. First, hire a professional. Second, file a Voluntary Disclosure Application.
+            </p>
+            <span>The Voluntary Disclosure Application Process</span>
+            <p>The Voluntary Disclosures Program &#40;VDP&#41; gives you a second chance to correct a tax return you previously filed or to file a return that you should have filed. If you file a VDP application and it is accepted by the Canada Revenue Agency &#40;CRA&#41; you will have to pay the taxes owing, plus interest in part or in full. However, you would be eligible for relief from prosecution and, in some cases, from penalties that you would otherwise be required to pay.
+            At the Tax Mechanic we handle the whole process for you seamlessly. The first step is filing an RC199. Then we prepare your returns and draft a strategy for repayment. Revenue Canada will assess your returns and voila, you are up to date. You don’t have to visit a brick and mortar tax shop or talk to any annoying CRA officers. <br/>
+            Through the VDP, taxpayers can make an application to correct inaccurate or incomplete information, or to disclose information not previously reported. For example, taxpayers may not have met their tax obligations if they claimed ineligible expenses, failed to remit source deductions, did not file an information return, or underreported their income.
+            Effective March 1, 2018, VDP applications relating to income tax disclosures may fall into one of two tracks: <br />
+            The first track is a General Program. If accepted under the VDP, these applications will be eligible for penalty relief and partial interest relief. <br />
+            The second track is a Limited Program. Applications that disclose non-compliance where there is an element of intentional conduct on the part of the taxpayer or a closely related party will be processed under this Limited Program and if accepted, will receive reduced relief under the VDP.
+            The VDP promotes compliance with Canada’s tax laws by encouraging taxpayers to voluntarily come forward and correct any previous errors or omissions in their tax affairs. If a VDP application is accepted by the CRA, taxpayers will have to pay the taxes owing, plus interest in part or in full. However, taxpayers would be eligible for relief from prosecution and, in some cases, from penalties that they would otherwise be subject to under the legislation. <br />
+            The Minister is not required to grant relief in respect of all applications made to the VDP. Each request will be reviewed and decided on its own merits. In exercising discretion, the Minister is guided by principles of procedural fairness which require decisions to be made in good faith, in a manner that promotes the objects of the ITA. This information circular provides general guidelines on some of the relevant considerations in the exercise of this discretion. If relief is denied, the CRA will provide the taxpayer with an explanation of the reasons for the decision. In such cases, relief of arrears interest and any penalties payable may be requested and considered in accordance with the taxpayer relief provisions as described in IC 07-1R1, Taxpayer Relief Provisions.</p>
+            <p>
+            <span>Contact</span>
+            <br/>
+            I have successfully filed hundreds of VDP applications with a 100% success rate!
+            <br/>
+            If you have any questions about the process you can contact;
+            <br/><br/>
+            Tax Mechanic<br/>
+            180 John St<br/>
+            Toronto, ON<br/>
+            M5T 1X5<br/>
+            taxmechanic.ca<br/>
+            647-499-5693<br/>
+            david@taxmechanic.ca<br/>
+            </p>
+            </div>
+            }/>
+        </div>
+    )
+}
