256d48aeb5dbbb495a41603429e9519383a97c1e
HerilS <herilsaha06@gmail.com>
2020-05-25 17:21:27 -0600

    Finished Blog Page 2 Ninth Section

--- src/components/Blog/Page2/P9-Page2.js
+++ src/components/Blog/Page2/P9-Page2.js
@@ -9,17 +9,59 @@ export default function P9Page2() {
             <img className="GradientS1Comp" src="/img/OurTeam/TopImg.png" width="100%" alt="OurTeam"/>
                 <div className="S1TextContainer">
                     <div className="S1Text">
-                        <span style={{whiteSpace: "normal"}}>Underground Economy In a Pandemic</span>
+                        <span style={{whiteSpace: "normal"}}>Did you get a Bonus?</span>
                         <div className="ButtonContainerS1">
                         <Button className="Button" text="Book For a Free Consultation Today"/>
                         </div>
                     </div>
                 </div>
             </div>
-            <BlogPages date="May 19, 2020" title="Underground Economy In A Pandemic" subtitle="By: Fraser Simpson" mainPara=
+            <BlogPages date="February 7, 2018" title="Did you get a bonus?" subtitle="By: Fraser Simpson" mainPara=
             {
             <div>
-            
+            <p>
+            Did you get a bonus? The Canada Revenue Agency (CRA) did for $35,000. Five things you can do right now to get YOUR bonus.
+            <br/><br/>
+            CRA’s top executives pocketed an average of $35,000 in performance pay for 2017! Ever try to call the CRA? Performance is not the adjective I would use, more like incompetent.
+            <br/><br/>
+            The total amount spent on performance pay for CRA executives was $6.8 million in 2015/16 and $6.9 million in 2016/17.
+            <br/><br/>
+            You can get mad or you can get even.
+            <br/><br/>
+            Here’s how to get even;
+            <br/>
+            </p>
+            <ul>
+                <li>Lower your tax payable to give yourself a bonus? How? Hire a professional.</li>
+                <li>File on time-no need to give the CRA extra money-they will only increase their bonus.</li>
+                <li>Start a side business-you can use the write-off’s to offset other income-see #1 above.</li>
+                <li>Max out your TFSA and RRSP’s-duh.</li>
+                <li>Call us.</li>
+            </ul>
+            <p>
+            Tax is your biggest expense. You need to manage this expense with the same focus you would on other expenses like insurance, medical, education, etc.
+            <br/><br/>
+            Karl Lavoie, CRA spoke person, said executives are assessed according to the government-wide priorities such as healthier workplaces and diversity as well as the agency’s priorities.
+            <br/><br/>
+            But critics question why the CRA’s top executives are getting so much in performance pay when the agency is under fire for its performance.
+            <br/><br/>
+            For example, they cite a damning auditor general’s report that found the agency couldn’t handle high call volumes, often didn’t answer the phone and gave taxpayers incorrect information 30 per cent of the time.
+            <br/><br/>
+            If you have called the CRA lately they usually respond with a voice message declaring that they are too busy and to call back later? That’s performance?
+            <br/><br/>
+            New Democratic Party Revenue critic Pierre-Luc Dusseault questions whether such large performance paycheques are justified, given the problems at the CRA in recent years. (The baby minister-he’19! Our future is in good hands).
+            </p>
+            <p>
+            Dusseault said he was very surprised top CRA executives are getting that much performance pay — particularly given the problems with the call centre.
+            <br/><br/>
+            “Many Canadians have found there were many problems with the CRA, many problems getting services from the CRA and now we see those executives (getting) money for their performance when Canadians don’t think the performance was good in the last two years.”
+            <br/><br/>
+            Dusseault also called into question the CRA’s performance when it comes to taking action on offshore tax evasion in the wake of the Panama Papers, the Paradise Papers and other leaks of offshore tax account information.
+            <br/><br/>
+            “When we look at other tax agencies around the world, they are much better at going after tax cheats.”
+            <br/><br/>
+            If the CRA deserves a bonus-surely you do. Call the Tax Mechanic for a tune-up.  
+            </p>
             <p>
             <span>Contact</span>
             <br/>
