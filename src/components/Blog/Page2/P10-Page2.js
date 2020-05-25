6388016851a774235e4c6588ea4066f6faa54ee9
HerilS <herilsaha06@gmail.com>
2020-05-25 17:22:47 -0600

    Finished Blog Page 2 Tenth Section

--- src/components/Blog/Page2/P10-Page2.js
+++ src/components/Blog/Page2/P10-Page2.js
@@ -9,17 +9,29 @@ export default function P10Page2() {
             <img className="GradientS1Comp" src="/img/OurTeam/TopImg.png" width="100%" alt="OurTeam"/>
                 <div className="S1TextContainer">
                     <div className="S1Text">
-                        <span style={{whiteSpace: "normal"}}>Underground Economy In a Pandemic</span>
+                        <span style={{whiteSpace: "normal"}}>CRA Tax Payment Plans</span>
                         <div className="ButtonContainerS1">
                         <Button className="Button" text="Book For a Free Consultation Today"/>
                         </div>
                     </div>
                 </div>
             </div>
-            <BlogPages date="May 19, 2020" title="Underground Economy In A Pandemic" subtitle="By: Fraser Simpson" mainPara=
+            <BlogPages date="January 18, 2018" title="CRA Tax Payment Plans" subtitle="By: Fraser Simpson" mainPara=
             {
             <div>
-            
+            <p>
+            A payment arrangement is an agreement you make with the CRA. It allows you to make smaller payments over time until you have paid your entire debt including applicable interest.
+            <br/><br/>
+            Before you make a payment arrangement, you may need to show that you have tried to pay your debt in full by borrowing money or reducing your expenses. To figure out your ability to pay, they may ask you to provide proof of your income, expenses, assets, and liabilities. You may have to do this by telephone or by completing a financial questionnaire.
+            <br/><br/>
+            Some people simply have tax debt and don’t have the money to pay it. Over time, interest continues to grow and the tax debt becomes even more difficult to pay. Some people get behind filing tax returns, knowing that they will soon have a tax debt that they will not be in a position to pay off.
+            <br/><br/>
+            If you know that a tax debt is owed or that a tax debt will be owed once you become compliant (i.e. file the required returns) and are just simply not in a position to pay it, then what you have or are about to have is a financial problem.
+            <br/><br/>
+            Trying to make a CRA tax payment plan directly with the CRA (Canada Revenue Agency) is dangerous. This is because, in consideration of accepting a short term payment plan from you, they will often want significant financial disclosure, such as where you bank, where you work and where you live. This is in the hope that in the future, when your CRA tax payment plan expires or if you miss a payment, they can use enforcement action on your income, home or bank account to force you to pay your tax debt in full.
+            <br/><br/>
+            We often negotiate CRA tax payment plans for our clients. In addition, we offer a host of other financial resources to those who want to raise the funds to pay off the CRA once and for all or for those who can’t pay and need financial relief.
+            </p>
             <p>
             <span>Contact</span>
             <br/>
