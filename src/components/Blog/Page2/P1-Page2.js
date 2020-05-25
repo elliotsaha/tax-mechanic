b76b9f1c3a35ca79a851aed65fa086d4ecc0c262
HerilS <herilsaha06@gmail.com>
2020-05-24 19:10:57 -0600

    Finished Page 2 Section 1

--- src/components/Blog/Page2/P1-Page2.js
+++ src/components/Blog/Page2/P1-Page2.js
@@ -19,7 +19,20 @@ export default function P1Page2() {
             <BlogPages date="May 19, 2020" title="Underground Economy In A Pandemic" subtitle="By: Fraser Simpson" mainPara=
             {
             <div>
-            
+            <p>
+            In its continued campaign to monitor what it called “compliance risks” in Canada’s hottest housing markets, the Canada Revenue Agency has uncovered nearly $600 million in unpaid taxes in the British Columbia and Ontario real estate segments.
+            <br/><br/>
+            CRA auditors, which have been closely examining real estate transactions since 2015, found that the B.C. market has $169 million in unpaid taxes from home sales, while Ontario has $423 million.
+            <br/><br/>
+            The federal bureau further noted that in B.C., 54% of the unpaid amount was due to buyers not paying the GST, while 45% was from income tax. In Ontario, fully 90% of the unpaid taxes represented GST.
+            <br/><br/>
+            The CRA stated that it is keeping a particularly close eye at the factors that tend to indicate tax evasion, such as unreported GST, capital gains, or worldwide income, as well as property flipping and questionable sources of funding for purchases.
+            <br/><br/>
+            Fraser Simpson, one of Canada’s leading tax authorities, offers solutions to tax problems using the Voluntary Disclosure Program, Tax Court hearings, and Taxpayer Relief Provisions under the Income Tax Act.
+            <br/><br/>
+            Whether it is business or personal tax issues the Tax Mechanic is here to help you. We offer a FREE 30-minute consultation and with over 30 years of professional tax filing and negotiation experience we can work with you to get your life back!
+            </p>
+            <br/><br/>
             <p>
             <span>Contact</span>
             <br/>
