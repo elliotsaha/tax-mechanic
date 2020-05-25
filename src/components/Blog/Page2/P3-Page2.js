8a2317fd980e03b9e1e4f7dfce77b326d858544b
HerilS <herilsaha06@gmail.com>
2020-05-24 19:36:56 -0600

    Finished Blog Page 2 Third Section

--- src/components/Blog/Page2/P3-Page2.js
+++ src/components/Blog/Page2/P3-Page2.js
@@ -9,17 +9,42 @@ export default function P3Page2() {
             <img className="GradientS1Comp" src="/img/OurTeam/TopImg.png" width="100%" alt="OurTeam"/>
                 <div className="S1TextContainer">
                     <div className="S1Text">
-                        <span style={{whiteSpace: "normal"}}>Underground Economy In a Pandemic</span>
+                        <span style={{whiteSpace: "normal"}}>A tax article that could help change your life for the better</span>
                         <div className="ButtonContainerS1">
                         <Button className="Button" text="Book For a Free Consultation Today"/>
                         </div>
                     </div>
                 </div>
             </div>
-            <BlogPages date="May 19, 2020" title="Underground Economy In A Pandemic" subtitle="By: Fraser Simpson" mainPara=
+            <BlogPages date="July 30, 2018" title="A tax article that could help change your life for the better" subtitle="By: Fraser Simpson" mainPara=
             {
             <div>
-            
+            <p>
+            A number of months ago my business partner and tax expert, Fraser Simpson was featured in an article originally that ran in Money Sense and was picked up by Maclean’s. This article has been extremely helpful to those who have read it, so we thought it was time to share some of the highlights that could help if you have a current tax problem. Here are a couple of highlights…. 
+            </p>
+            <span>Why people put off filing tax returns?</span>
+            <p>
+            Once someone skips one year they tend to skip a second year and before you know it five years pass by. But with today’s sophisticated technology the CRA is ramping up its search and is going after tax money and taxpayers everywhere.
+            </p>
+            <span>Who are the most likely not to file?</span>
+            <p>The self-employed who have no T4 slip, often let tax reporting slide off their pates. Either they get to busy or they won’t have the money to pay the taxes they owe. Tax debts can easily snowball. The good news is tax professionals often approach the CRA on your behalf and set up a manageable payment plan.
+            <br/><br/>
+            Other questions that were posed to Fraser in the article:
+            </p>
+            <span>So what should I do to file overdue tax returns without penalty?</span>
+            <br/><br/>
+            <span>Can you give us an example of what those penalties would be?</span>
+            <br/><br/>
+            <span>Can taxpayers do this themselves?</span>
+            <br/><br/>
+            <span>When does it really become a problem?</span>
+            <br/><br/>
+            <span>But isn’t it true that the CRA can’t go back more than three years to get tax they’re owed?</span>
+            <br/><br/>
+            <span>What should taxpayers know about payment plans?</span>
+            <br/><br/>
+            <span>What is a ‘notional assessment’ and where does it happen?</span>
+            <br/><br/>
             <p>
             <span>Contact</span>
             <br/>
