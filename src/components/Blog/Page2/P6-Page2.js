f75ad369d2df728775bbb1c8c48e6207292020fd
HerilS <herilsaha06@gmail.com>
2020-05-24 22:08:33 -0600

    Finished Blog Page 2 Sixth and Seventh Section

--- src/components/Blog/Page2/P6-Page2.js
+++ src/components/Blog/Page2/P6-Page2.js
@@ -9,17 +9,25 @@ export default function P6Page2() {
             <img className="GradientS1Comp" src="/img/OurTeam/TopImg.png" width="100%" alt="OurTeam"/>
                 <div className="S1TextContainer">
                     <div className="S1Text">
-                        <span style={{whiteSpace: "normal"}}>Underground Economy In a Pandemic</span>
+                        <span style={{whiteSpace: "normal"}}>The Psychology of Why People don’t File Their Taxes</span>
                         <div className="ButtonContainerS1">
                         <Button className="Button" text="Book For a Free Consultation Today"/>
                         </div>
                     </div>
                 </div>
             </div>
-            <BlogPages date="May 19, 2020" title="Underground Economy In A Pandemic" subtitle="By: Fraser Simpson" mainPara=
+            <BlogPages date=" April 13, 2018" title="The Psychology of Why People don’t File Their Taxes" subtitle="By: Fraser Simpson" mainPara=
             {
             <div>
-            
+            <p>
+            The psychology of why people don’t file their taxes is a complex, often stress-filled, issue. Reasons often come with strong negative emotions. One year of unfiled taxes with the CRA, can turn into two, then three, and before you know it even 10 years.  Serious problems are created, financially and emotionally, including fear, anger, anxiety and guilt. Whether you are a business owner or an employee, tax filing is necessary and our duty as a citizen of Canada.
+            <br/><br/>
+            Every day, we speak to people struggling with paying their taxes and filing on time. Please watch our video where I speak with a good friend, Jennifer Brighton, MSW, RSW, MPEC, Psychotherapist, Family Counselor and Life Coach. Jennifer’s practice history includes people who have not made their annual tax filing a priority. A Tax Cloud gathers.
+            <br/><br/>
+            Jennifer shares a story of a couple suffering relationship issues, including a serious lack of tax filing, having not filed in seven years. Initially, family drama starts them on the “no tax filing path”, then “tax time” brings a cloud of “life-limiting” fears, anxieties and horrible embarrassment. They realize they could not manage their seven year tardiness in tax filing on their own and hire a new member to their team to sort this out. The stress filled, seven year drama ends with money back from the CRA. A Tax Cloud is lifted!
+            <br/><br/>
+            Jennifer, reflects that the fear of the CRA, and not filing for seven years was a real relationship drama for this couple. Jennifer describes FEAR as False Expectations Appearing Real. FEAR creates a terrifying false idea in the mind that the CRA and filing taxes is an insurmountable obstacle. Not filing taxes can completely overwhelm people and shut them down to the point where they can find themselves years behind. In reality the facts are that there are companies out there that can help.
+            </p>
             <p>
             <span>Contact</span>
             <br/>
--- src/components/Blog/Page2/P7-Page2.js
+++ src/components/Blog/Page2/P7-Page2.js
@@ -9,17 +9,33 @@ export default function P7Page2() {
             <img className="GradientS1Comp" src="/img/OurTeam/TopImg.png" width="100%" alt="OurTeam"/>
                 <div className="S1TextContainer">
                     <div className="S1Text">
-                        <span style={{whiteSpace: "normal"}}>Underground Economy In a Pandemic</span>
+                        <span style={{whiteSpace: "normal"}}>Summer Is Here! But Your Bank Account Might Still Be Frozen</span>
                         <div className="ButtonContainerS1">
                         <Button className="Button" text="Book For a Free Consultation Today"/>
                         </div>
                     </div>
                 </div>
             </div>
-            <BlogPages date="May 19, 2020" title="Underground Economy In A Pandemic" subtitle="By: Fraser Simpson" mainPara=
+            <BlogPages date="May 28, 2018" title="Summer Is Here! But Your Bank Account Might Still Be Frozen" subtitle="By: Fraser Simpson" mainPara=
             {
             <div>
-            
+            <p>
+            Last month we were contacted by Jason and Carol from Alberta. They own a business that has 25 employees, and over the years the company had done very well. A few years ago some health and personal issues changed things significantly. Tax and GST returns were not filed and before they new it the CRA had frozen their bank accounts. The business, their livelihood and their employee’s jobs were in serious jeopardy.
+            <br/><br/>
+            Every week we speak to individuals like Jason and Carol who have had their bank accounts frozen by Revenue Canada. Many know how they got to that point, and usually it is a series of phone calls and letters from the CRA. There are basically four key stages.
+            </p>
+            <span>Stage One</span>
+            <p>you will be notified in writing that an amount is owing. At this point it is a good time to call us. This initial escalation it is not yet reached the “FROZEN BANK ACCOUNT” stage and this letter in many cases might just be ignored… we strongly recommend to not ignore it and talk to us. Remember even if you disagree with the amount interest and penalties will continue to rise.</p>
+            <span>Stage Two</span>
+            <p>
+            you will be sent another letter that will be much more demanding that you pay the amount owning. Again many ignore this letter too, thinking maybe this will just go away. Trust us, they will not go away.
+            </p>
+            <span>Stage Three</span>
+            <p>
+            you might receive a phone call from a CRA agent usually at this stage it is now in collections….NOT GOOD!! …and if you continue to ignore it, comes to the final stage.
+            </p>
+            <span>Stage Four</span>
+            <p>a requirement to pay letter is sent to your bank, requiring the bank to freeze your bank account. If you are employed your employer might receive a requirement to garnish your wages. This no doubt will be a red flag with your employer. The bank institution you have been dealing with perhaps for years could be severely damaged.</p>
             <p>
             <span>Contact</span>
             <br/>
