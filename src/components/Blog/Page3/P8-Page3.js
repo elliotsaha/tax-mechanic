19ea649b705c8169c1f1eccae1e101b5c6b6101e
HerilS <herilsaha06@gmail.com>
2020-05-25 18:12:08 -0600

    Finished Page 3 Section 8 Blog

--- src/components/Blog/Page3/P8-Page3.js
+++ src/components/Blog/Page3/P8-Page3.js
@@ -16,10 +16,24 @@ export default function P8Page3() {
                     </div>
                 </div>
             </div>
-            <BlogPages date="October 23, 2018" title="Lower your Tax Debt" subtitle="By: Fraser Simpson" mainPara=
+            <BlogPages date="September 18, 2017" title="Not Filing Your Taxes? It Will Be Ineligible For More Than Just Credit" subtitle="By: Fraser Simpson" mainPara=
             {
             <div>
-            
+            <p>
+            Each year thousands of individuals and businesses don’t file their taxes on time or they just don’t file at all. Most people know that not filing leads to a number of problems. Something that you may or not be aware of that failing to file puts you at risk at receiving federal and provincial benefits that are calculated on annual tax returns.
+            <br/><br/>
+            Among the benefits calculated based on tax returns are child tax credits, Ontario Trillium benefits, mortgages, loans, transfer of properties passing through probate and the list goes on.
+            <br/><br/>
+            We have many clients who have been relying on for example child tax credits having them cut off and only because of non-filing. They then have to wait until returns are filed and they are reinstated back into the system.
+            <br/><br/>
+            On top of the problems that come with non-filing there are penalties and interest that will mount. Five percent penalty on taxes owed, one percent is added each month up to 12 months. Interest is also charged, compounded daily.
+            <br/><br/>
+            Taxpayers, for example, who owe $10,000 in taxes would face over $2,000 in penalties and interest after one year. Right now through the Voluntary Disclosure Program these penalties could be waived but changes to this program are coming in January 2018. Watch our video to find out more.
+            <br/><br/>
+            Now most income forms are transferred directly to the government, where computers help determine which late filer to pursue based on their tax recovery potential.
+            <br/><br/>
+            Individuals and business that we speak to are almost always worried about what this is going to cost them. Not so much our fees, that are very reasonable, but how are they are going to pay CRA for the amount owing. We have negotiated on behalf of many clients payment arrangements with the CRA that are affordable and manageable based on one’s ability to pay..
+            </p>
             <p>
             <span>Contact</span>
             <br/>
