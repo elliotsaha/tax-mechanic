bd2a29e477b114fc72719c86b5b5a7fab5f19b04
HerilS <herilsaha06@gmail.com>
2020-05-25 20:13:42 -0600

    Finished Page 5 Section 4

--- src/components/Blog/Page5/P4-Page5.js
+++ src/components/Blog/Page5/P4-Page5.js
@@ -16,10 +16,27 @@ export default function P4Page5() {
                     </div>
                 </div>
             </div>
-            <BlogPages date=" September 9, 2017" title="A Tax Horror Story With A Happy Ending." subtitle="By: Fraser Simpson" mainPara=
+            <BlogPages date="March 20, 2017" title="Whitby Resident Jailed And Fined For Tax Evasion" subtitle="By: Fraser Simpson" mainPara=
             {
             <div>
-            
+            <p>
+            Newmarket, Ontario, January 23, 2017
+            <br/><br/>
+            The Canada Revenue Agency (CRA) announced today that on October 20, 2016, Wolfgang John Wilm of Whitby, Ontario was found guilty in the Ontario Court of Justice in Newmarket, of one count of tax evasion under the Income Tax Act. He was sentenced on January 20, 2017, to 20 months in jail and was fined a total of $552,976. In addition to the court imposed fine, Mr Wilm will also have to pay the full amount of tax owing, plus related interest and any penalties assessed by the CRA.
+            <br/><br/>
+            A CRA investigation revealed that from 2007 to 2010 Wilm failed to file personal income tax returns to report income he had earned during those years. Wilm received a total of $2,081,167 in income as a self-employed individual which he failed to report. By not filing his returns on time and as required, Wilm willfully evaded or attempted to evade federal taxes of $552,976.
+            <br/><br/>
+            The preceding information was obtained from the court records.
+            <br/><br/>
+            “Our taxes pay for the high level of government services that Canadians count on,” said Vince Pranjivan, Assistant Commissioner of the Ontario Region. “When someone tries to evade taxes, it affects all Canadians.”
+            <br/><br/>
+            When taxpayers are convicted of income tax evasion, they still must repay the full amount of taxes owing, plus interest and any civil penalties that may be assessed by the CRA. In addition, the court may fine them up to 200% of the taxes evaded and impose a jail term of up to five years.
+            <br/><br/>
+            Taxpayers who have not filed returns for previous years, or who have not reported all of their income, can still voluntarily correct their tax affairs. They may not be penalized or prosecuted if they make a valid disclosure before they become aware of any compliance action being initiated by the CRA against them.    
+            </p>
+            <p>
+            There are many factors to consider when filing multiple years’ tax returns and/or declaring unclaimed income. The most important thing is timing. If you contact the CRA and put them on notice that you will file up to date it could save you a significant penalty. However, if the CRA sends you a letter requesting compliance then you forfeit your right to voluntary disclosure and subject yourself to a negligence penalty which is equivalent to the tax you owe. Therefore doubling the amount you have to pay.  
+            </p>
             <p>
             <span>Contact</span>
             <br/>
