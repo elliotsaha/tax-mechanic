d7358aee1a1a19f424e8a3ca6f605d417e9a3149
HerilS <herilsaha06@gmail.com>
2020-05-25 19:21:30 -0600

    Finished Page 4 Blog Section 4

--- src/components/Blog/Page4/P4-Page4.js
+++ src/components/Blog/Page4/P4-Page4.js
@@ -9,17 +9,32 @@ export default function P4Page4() {
             <img className="GradientS1Comp" src="/img/OurTeam/TopImg.png" width="100%" alt="OurTeam"/>
                 <div className="S1TextContainer">
                     <div className="S1Text">
-                        <span style={{whiteSpace: "normal"}}>Tax Tips For AirBNB Owners And Uber Drivers</span>
+                        <span style={{whiteSpace: "normal"}}>CRA Aggressively Going After Taxpayers... What's Next?</span>
                         <div className="ButtonContainerS1">
                         <Button className="Button" text="Book For a Free Consultation Today"/>
                         </div>
                     </div>
                 </div>
             </div>
-            <BlogPages date="January 10, 2018" title="Tax Tips For AirBNB Owners And Uber Drivers" subtitle="By: Fraser Simpson" mainPara=
+            <BlogPages date="July 17, 2017" title="CRA Aggressively Going After Taxpayers... What's Next?" subtitle="By: Fraser Simpson" mainPara=
             {
             <div>
-                
+            <p>
+            From April 1, 2012 to March 31, 2017, the CRA convicted 408 individuals and businesses. This involved approximately $122 million in federal tax evaded and court sentences totaling $44 million in court fines, and 3103 months in jail.
+            <br/><br/>
+            Between 2006 and 2017, 75 tax scheme promoters from across Canada were successfully convicted of tax offences related to tax schemes. This led to $7.15 million in court imposed fines and a total of 936 months of jail awarded.
+            <br/><br/>
+            On June 13, 2017, over 80 tax investigators from the CRA and the United Kingdom’s tax authority executed searches in both countries as part of an ongoing investigation of an alleged carousel tax fraud scheme against the Government of Canada.
+            <br/><br/>
+            Be aware of your reporting obligations
+            <br/><br/>
+            Taxpayers must report certain foreign assets with a total cost of more than $100,000 on Form T1135, Foreign Income Verification Statement, by the filing due date of their income tax return. They must also make sure that they have properly reported any foreign income and gains on their income tax return.
+            <br/><br/>
+            There is no question, with the enthusiastic support of the current government, CRA is going after delinquent taxpayers. Being taxpayers ourselves, when you look at the information above you would be thinking this is money that in many cases is owed, and should be paid. Absolutely. I think is also important to realize more than ever the importance of keeping your bookkeeping, payroll taxes, HST and corporate tax up to date if you own a business or if you have investments, foreign or domestic that they are reported properly.
+            <br/><br/>
+            At the Tax Mechanic we deal with a variety of tax and accounting issues. Businesses and individuals who work with us understand the importance of letting the experts get the job done so they can focus on other things.
+
+            </p>
             <p>
             <span>Contact</span>
             <br/>
