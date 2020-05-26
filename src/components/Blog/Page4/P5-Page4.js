837d236744693b000b91715eb7b60a1d61a87108
HerilS <herilsaha06@gmail.com>
2020-05-25 19:25:50 -0600

    Finished Page 4 Blog Section 5

--- src/components/Blog/Page4/P5-Page4.js
+++ src/components/Blog/Page4/P5-Page4.js
@@ -9,17 +9,35 @@ export default function P5Page4() {
             <img className="GradientS1Comp" src="/img/OurTeam/TopImg.png" width="100%" alt="OurTeam"/>
                 <div className="S1TextContainer">
                     <div className="S1Text">
-                        <span style={{whiteSpace: "normal"}}>Tax Tips For AirBNB Owners And Uber Drivers</span>
+                        <span style={{whiteSpace: "normal"}}>New CRA Rules Regarding The Voluntary Disclosure Program To Start January 1, 2018</span>
                         <div className="ButtonContainerS1">
                         <Button className="Button" text="Book For a Free Consultation Today"/>
                         </div>
                     </div>
                 </div>
             </div>
-            <BlogPages date="January 10, 2018" title="Tax Tips For AirBNB Owners And Uber Drivers" subtitle="By: Fraser Simpson" mainPara=
+            <BlogPages date="July 17, 2017" title="New CRA Rules Regarding The Voluntary Disclosure Program To Start January 1, 2018" subtitle="By: Fraser Simpson" mainPara=
             {
             <div>
-                
+            <p>
+            Since the launch of Voluntary Disclosure Program (VDP) we have been able to help many individuals dealing with a variety of different financial and personal challenges to navigate their way through this program and successfully accomplish getting their taxes up to date.
+            <br/><br/>
+            To encourage individuals to become compliant this program was created by CRA and currently has benefits such as not having to file or pay for any back taxes for 90 days giving the taxpayer time to have these returns properly prepared and submitted, for the years that were not filed.  However, coming this January the rules for this program will be changing dramatically making it, according to CRA, a “fairer system”. In reality it will make it more difficult on the taxpayer both in registering for this program and if needed negotiating a payment arrangement needed to pay off a tax debt.
+            <br/><br/>
+            Revenue Canada looking for non-filers using updated technology, backed by new powers provided by Trudeau.
+            <br/><br/>
+            The proposed “draft” Information Circular (we fairly certain it is a done deal!) is now out on the new VDP rules.
+            <br/><br/>
+            The taxpayer must include payment of the estimated tax owing with their VDP application.
+            <br/><br/>
+            When the taxpayer does not have the ability to make payment of the estimated tax owing, a payment arrangement supported by adequate security may be considered in extraordinary circumstances with approval from the CRA Collections officials. In these circumstances, the taxpayer must make full disclosure and provide evidence of income, expenses, assets, and liabilities supporting the inability to make payment in full.
+            <br/><br/>
+            If an application is made prior to December 31, 2017 payment is not required.
+            <br/><br/>
+            Revenue Canada will continue to go after non-filers and with the improvement of the tools, technics and technologies being used they will eventually caught up to you.
+            <br/><br/>
+            If you have not filed your taxes in a few years or longer let us help you.
+            </p>
             <p>
             <span>Contact</span>
             <br/>
