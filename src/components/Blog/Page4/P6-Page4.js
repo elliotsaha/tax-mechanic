5431d4db5555f448d94091975c7a9aa72cf11199
HerilS <herilsaha06@gmail.com>
2020-05-25 19:28:59 -0600

    Finished Page 4 Blog Section 6

--- src/components/Blog/Page4/P6-Page4.js
+++ src/components/Blog/Page4/P6-Page4.js
@@ -16,10 +16,27 @@ export default function P6Page4() {
                     </div>
                 </div>
             </div>
-            <BlogPages date="January 10, 2018" title="Tax Tips For AirBNB Owners And Uber Drivers" subtitle="By: Fraser Simpson" mainPara=
+            <BlogPages date="July 1, 2017" title="Changes To The Voluntary Disclosure Program Coming. Time Is Now To File" subtitle="By: Fraser Simpson" mainPara=
             {
             <div>
-                
+            <p>
+            If you have not filed your taxes in a couple of years (or more) or ever omitted some income from your tax return, you may be interested in proposed changes announced last week to the Canada Revenue Agency’s Voluntary Disclosure Program (VDP). The VDP allows taxpayers to voluntarily come forward and file previous tax returns or correct previous tax filing errors without penalty or fear of prosecution. The tax would still be owing with interest; however, penalties and interest would be waived.
+            <br/><br/>
+            The proposed changes to the VDP were introduced back in October 2016, when the House of Commons Standing Committee on Finance, recommended that the CRA undertake a comprehensive review of the VDP. In December 2016, the committee concluded that the VDP could be made “more effective and fairer.” Proposals included a framework for tightening the criteria to be accepted into the program.
+            <br/><br/>
+            The CRA is expected to announce formal changes to the VDP in the fall of 2017. Any changes would be effective Jan. 1, 2018.
+            <br/><br/>
+            What does this mean for you?
+            <br/><br/>
+            Here's why;
+            </p>
+            <ul>
+                <li>The existing rules essentially make it easy to qualify for a penalty waiver and associated interest.</li>
+                <li>As the new rules are formed in the fall, the CRA may stall applications till the new year when the new rules come into force.</li>
+                <li>The new rules will make it harder to qualify.</li>
+                <li>The summer is the best time to file overdue taxes as the CRA reports to the government in October  and they want to show good numbers; i.e. collected tax from overdue returns.</li>
+            </ul>
+            <p>Changes include: requiring the payment of the estimated taxes owing as a condition of qualifying for the program. Currently, you do not have to pay anything, just report and make a payment arrangement.</p>
             <p>
             <span>Contact</span>
             <br/>
