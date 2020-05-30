0bd5e3d6b6e871eaa2f9c01e42024d85b50136e0
HerilS <herilsaha06@gmail.com>
2020-05-29 22:37:18 -0600

    Retrying Build Prod v1.0.0

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import ScrollToTop from 'react-router-scroll-top';
 import ScrollMemory from "react-router-scroll-memory";
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/React">
     <ScrollMemory />
       <ScrollToTop>
         <App />
