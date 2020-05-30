a52f369f8e5b93e13c30eb1fda44cfc952378eef
HerilS <herilsaha06@gmail.com>
2020-05-30 02:46:43 -0600

    Build Prod v1.0.0 Mobile Fix

---
+++ build/precache-manifest.7f69f2b95539b69b2fba8c4c663af8ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db741e2cb6eeb32f96fe5e2d1df2c2ec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db76071cfef2b748a3e3",
+    "url": "./static/css/2.6b5d2c53.chunk.css"
+  },
+  {
+    "revision": "20ed94a95009ab688e98",
+    "url": "./static/css/main.fae2085d.chunk.css"
+  },
+  {
+    "revision": "db76071cfef2b748a3e3",
+    "url": "./static/js/2.9160e97b.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "./static/js/2.9160e97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20ed94a95009ab688e98",
+    "url": "./static/js/main.24d66eed.chunk.js"
+  },
+  {
+    "revision": "4fd90b81228dc5abc28c",
+    "url": "./static/js/runtime-main.b753049a.js"
+  },
+  {
+    "revision": "a56aee7087116cd4307a619e9e01186d",
+    "url": "./static/media/City.a56aee70.jpg"
+  }
+]);
