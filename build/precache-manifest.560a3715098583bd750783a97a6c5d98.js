4038008fe20d78d947d21162fa3233e8db9132b7
HerilS <herilsaha06@gmail.com>
2020-05-30 00:34:40 -0600

    Retrying Deploy v1.0.0

---
+++ build/precache-manifest.560a3715098583bd750783a97a6c5d98.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a04884dd6e3486bf459204202ea297c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db76071cfef2b748a3e3",
+    "url": "./static/css/2.6b5d2c53.chunk.css"
+  },
+  {
+    "revision": "850375f618a1355873f7",
+    "url": "./static/css/main.08d24c7b.chunk.css"
+  },
+  {
+    "revision": "db76071cfef2b748a3e3",
+    "url": "./static/js/2.9160e97b.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "./static/js/2.9160e97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "850375f618a1355873f7",
+    "url": "./static/js/main.72a188fc.chunk.js"
+  },
+  {
+    "revision": "4fd90b81228dc5abc28c",
+    "url": "./static/js/runtime-main.b753049a.js"
+  },
+  {
+    "revision": "a56aee7087116cd4307a619e9e01186d",
+    "url": "./static/media/City.a56aee70.jpg"
+  }
+]);
