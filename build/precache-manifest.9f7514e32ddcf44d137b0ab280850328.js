fac3754bb2fd3e8ea460d41c5d96ba64990b9a18
HerilS <herilsaha06@gmail.com>
2020-06-01 15:00:20 -0600

    Fixed Bugs For Mobile and Typos Build Prod v1.0.2

---
+++ build/precache-manifest.9f7514e32ddcf44d137b0ab280850328.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d3321edceee4ed8b633ba73232ce77",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d64f7cbf318a671dca79",
+    "url": "./static/css/2.6b5d2c53.chunk.css"
+  },
+  {
+    "revision": "02873cf9f6d9ee3942e5",
+    "url": "./static/css/main.b08b78bc.chunk.css"
+  },
+  {
+    "revision": "d64f7cbf318a671dca79",
+    "url": "./static/js/2.7ea80858.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "./static/js/2.7ea80858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02873cf9f6d9ee3942e5",
+    "url": "./static/js/main.7d32bc75.chunk.js"
+  },
+  {
+    "revision": "4fd90b81228dc5abc28c",
+    "url": "./static/js/runtime-main.b753049a.js"
+  },
+  {
+    "revision": "a56aee7087116cd4307a619e9e01186d",
+    "url": "./static/media/City.a56aee70.jpg"
+  }
+]);
