93a479815ccf7607334c96c6b626bc420a9db55c
HerilS <herilsaha06@gmail.com>
2020-06-01 15:05:30 -0600

    Added Logo Link Fix on Mobile Navbar Build Prod v1.0.2

---
+++ build/precache-manifest.914c2b3b45636b7ee77942a72d45b975.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18663c36e159e0474a7677890e2647da",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d64f7cbf318a671dca79",
+    "url": "./static/css/2.6b5d2c53.chunk.css"
+  },
+  {
+    "revision": "e3836bf37ef9dd702496",
+    "url": "./static/css/main.b08b78bc.chunk.css"
+  },
+  {
+    "revision": "d64f7cbf318a671dca79",
+    "url": "./static/js/2.7ea80858.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "./static/js/2.7ea80858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3836bf37ef9dd702496",
+    "url": "./static/js/main.3f0f1a04.chunk.js"
+  },
+  {
+    "revision": "4fd90b81228dc5abc28c",
+    "url": "./static/js/runtime-main.b753049a.js"
+  },
+  {
+    "revision": "a56aee7087116cd4307a619e9e01186d",
+    "url": "./static/media/City.a56aee70.jpg"
+  }
+]);
