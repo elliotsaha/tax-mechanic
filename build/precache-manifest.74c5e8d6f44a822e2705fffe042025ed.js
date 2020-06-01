558cbfd9f4dc652831aceca7ce6d2a1ebfe17f8f
HerilS <herilsaha06@gmail.com>
2020-06-01 14:01:10 -0600

    Added dash to e-book link Build Prod v1.0.1

---
+++ build/precache-manifest.74c5e8d6f44a822e2705fffe042025ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a599cfe4a5b33925fcdd3cf17353b08e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db76071cfef2b748a3e3",
+    "url": "./static/css/2.6b5d2c53.chunk.css"
+  },
+  {
+    "revision": "c0a2547224a9350753b3",
+    "url": "./static/css/main.b08b78bc.chunk.css"
+  },
+  {
+    "revision": "db76071cfef2b748a3e3",
+    "url": "./static/js/2.9160e97b.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "./static/js/2.9160e97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a2547224a9350753b3",
+    "url": "./static/js/main.f79af0e2.chunk.js"
+  },
+  {
+    "revision": "4fd90b81228dc5abc28c",
+    "url": "./static/js/runtime-main.b753049a.js"
+  },
+  {
+    "revision": "a56aee7087116cd4307a619e9e01186d",
+    "url": "./static/media/City.a56aee70.jpg"
+  }
+]);
