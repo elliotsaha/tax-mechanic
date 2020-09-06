6122f068ccede5f8eb4dee6222dc415c1edde406
HerilS <herilsaha06@gmail.com>
2020-09-06 00:05:29 -0600

    finished panel four of homepage

--- src/components/home/PanelFour.tsx
+++ src/components/home/PanelFour.tsx
@@ -1,14 +1,152 @@
-import React from 'react'
+import React from "react"
 import { createStyles, makeStyles } from "@material-ui/core/styles"
-
+import Slider from "react-slick"
+import { Link, graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+import Button from "@material-ui/core/Button"
 const useStyles = makeStyles((theme: any) =>
   createStyles({
-      
-  }))
+    root: {
+      overflowX: "hidden",
+      background: "black",
+      color: "white",
+      paddingTop: "3rem",
+      fontFamily: "Gilroy, sans-serif",
+    },
+    mainTitle: {
+      fontWeight: "bolder",
+      textAlign: "center",
+      fontSize: "4rem",
+      marginBottom: "4rem",
+      lineHeight: "3rem",
+    },
+    cardRoot: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      textAlign: "center",
+    },
+    cardInner: {
+      maxWidth: "25rem",
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+    cardImage: {
+      borderRadius: "360rem",
+      borderStyle: "solid",
+      borderWidth: "7px",
+      borderColor: "#FEA700",
+    },
+    cardTitle: {
+      fontWeight: 600,
+      fontSize: "2.3rem",
+      marginTop: "2.5rem",
+      lineHeight: "3rem",
+      whiteSpace: "nowrap",
+    },
+    cardParagraph: {
+      fontSize: "0.95rem",
+    },
+    button: {
+      fontFamily: "Gilroy, sans-serif",
+      fontWeight: 600,
+      color: "white",
+      background: "linear-gradient(to right, #FEA700, #d98e00) no-repeat",
+      padding: "0.8rem",
+      textTransform: "none",
+      fontSize: "1rem",
+      paddingRight: "1.25rem",
+      paddingLeft: "1.25rem",
+      [theme.breakpoints.down(400)]: {
+        fontSize: "0.9rem",
+        padding: "0.65rem",
+      },
+    },
+    buttonContainer: {
+      paddingTop: '3rem',
+      paddingBottom: '3rem',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  })
+)
+
 export default function PanelFour() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/(content)/(ourTeam)/" } }
+        sort: { fields: [frontmatter___title], order: ASC }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              image {
+                childImageSharp {
+                  fluid(maxWidth: 450, quality: 100) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+            rawMarkdownBody
+          }
+        }
+      }
+    }
+  `)
+  const classes = useStyles()
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    pauseOnHover: false,
+    autoplaySpeed: 1000,
+    centerMode: true,
+    centerPadding: "0px",
+    touchMove: false,
+    swipe: false,
+  }
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.mainTitle}>Meet The Team</div>
+      <Slider {...settings}>
+        {data.allMarkdownRemark.edges.map(obj => {
+          return (
+            <div key={obj.node.frontmatter.title} className={classes.cardRoot}>
+              <div className={classes.cardInner}>
+                <div>
+                  <Img
+                    className={classes.cardImage}
+                    fluid={obj.node.frontmatter.image.childImageSharp.fluid}
+                  />
+                </div>
+                <div className={classes.cardTitle}>
+                  {obj.node.frontmatter.title}
+                </div>
+                <div className={classes.cardParagraph}>
+                  {obj.node.rawMarkdownBody.replace(/^(.{230}[^\s]*).*/, "$1")}
+                  ...
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </Slider>
+      <div className={classes.buttonContainer}>
+        <Link to="/team">
+          <Button className={classes.button}>Read More &rarr;</Button>
+        </Link>
+      </div>
+    </div>
+  )
 }
--- src/components/home/PanelThree.tsx
+++ src/components/home/PanelThree.tsx
@@ -1,49 +1,14 @@
-import React from "react"
+import React from 'react'
 import { createStyles, makeStyles } from "@material-ui/core/styles"
-import Slider from "react-slick"
 
-const useStyles = makeStyles((theme: any) => createStyles({
-  root: {
-    overflowX: 'hidden',
-  },
-}))
-
-export default function PanelThree() {
-  const classes = useStyles()
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: false,
-    autoplay: true,
-    pauseOnHover: false,
-    autoplaySpeed: 1000
-  }
-
-  return (
-    <div className={classes.root}>
-      <Slider {...settings}>
+const useStyles = makeStyles((theme: any) =>
+  createStyles({
+      
+  }))
+export default function PanelFour() {
+    return (
         <div>
-          <h3>1</h3>
+            
         </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
-    </div>
-  )
+    )
 }
