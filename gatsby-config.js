fd68f238f2fa50b058b0c982ecbdb5d22f1c81d3
HerilS <herilsaha06@gmail.com>
2020-09-05 19:04:12 -0600

    added netlify identity widget

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-netlify-cms`,
+    'gatsby-plugin-netlify-identity-widget',
     {
       resolve: `gatsby-plugin-material-ui`,
       options: {
