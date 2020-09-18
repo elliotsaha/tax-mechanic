9d10ab77d7566ac5f254556e47ba19fc2e4bf40f
HerilS <herilsaha06@gmail.com>
2020-09-17 19:11:16 -0600

    Testimonials netlify cms created

---
+++ src/pages/testimonials.js
@@ -0,0 +1,12 @@
+import React from "react"
+import PanelOne from "../components/testimonials/PanelOne"
+import Layout from "../components/layout"
+export default function testimonials() {
+  return (
+    <Layout>
+      <div>
+        <PanelOne />
+      </div>
+    </Layout>
+  )
+}
